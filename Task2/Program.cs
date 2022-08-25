53fb4d3481e954a18da04977df0ae9ae6a0f1f70
Yuliya Borovaya <Liana-rus81@mail.ru>
2022-08-25 19:14:10 +0300

    Task 2

---
+++ Task2/Program.cs
@@ -0,0 +1,42 @@
+﻿int a = 5;
+int b = 7;
+int c = 2;
+int d = 10;
+int e = -9;
+int f = -3;
+
+if (a > b)
+{
+    Console.WriteLine("{0} наибольшее число", a);
+    Console.WriteLine("{0} наименьшее число", b);
+}
+else
+{
+    Console.WriteLine("{0} наибольшее число ", b);
+    Console.WriteLine("{0} наименьшее число ", a);
+}
+Console.ReadLine();
+
+if (c > d)
+{
+    Console.WriteLine("{0} наибольшее число", c);
+    Console.WriteLine("{0} наименьшее число", d);
+}
+else
+{
+    Console.WriteLine("{0} наибольшее число ", d);
+    Console.WriteLine("{0} наименьшее число ", c);
+}
+Console.ReadLine();
+
+if (e > f)
+{
+    Console.WriteLine("{0} наибольшее число", e);
+    Console.WriteLine("{0} наименьшее число", f);
+}
+else
+{
+    Console.WriteLine("{0} наибольшее число ", f);
+    Console.WriteLine("{0} наименьшее число ", e);
+}
+Console.ReadLine();
